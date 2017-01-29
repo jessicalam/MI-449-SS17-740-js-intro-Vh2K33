de4c89d516ba1a2b4f8b7e3f14df50915fcf0aec
jessicalam <j.lam@mail.com>
2017-01-29 13:20:18 -0500

    first commit

---
+++ sounds.js
@@ -0,0 +1,39 @@
+function PlaySound1(melody) {
+    var audio = new Audio('drum-sounds/hihat.wav');
+    audio.play();
+}
+
+function PlaySound2(melody) {
+    var audio = new Audio('drum-sounds/tom.wav');
+    audio.play();
+}
+function PlaySound3(melody) {
+    var audio = new Audio('drum-sounds/openhat.wav');
+    audio.play();
+}
+function PlaySound4(melody) {
+    var audio = new Audio('drum-sounds/boom.wav');
+    audio.play();
+}
+
+function PlaySound5(melody) {
+    var audio = new Audio('drum-sounds/clap.wav');
+    audio.play();
+}
+function PlaySound6(melody) {
+    var audio = new Audio('drum-sounds/kick.wav');
+    audio.play();
+}
+function PlaySound7(melody) {
+    var audio = new Audio('drum-sounds/ride.wav');
+    audio.play();
+}
+
+function PlaySound8(melody) {
+    var audio = new Audio('drum-sounds/snare.wav');
+    audio.play();
+}
+function PlaySound9(melody) {
+    var audio = new Audio('drum-sounds/tink.wav');
+    audio.play();
+}
