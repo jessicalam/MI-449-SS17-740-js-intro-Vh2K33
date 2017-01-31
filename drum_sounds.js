7b0bfec0910405964f8954cdfa8926eb438cd8ac
jessicalam <j.lam@mail.com>
2017-01-31 16:03:38 -0500

    probably the right way to complete the project

---
+++ drum_sounds.js
@@ -0,0 +1,62 @@
+var buttonElement = document.getElementById('hihat')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+  var audio = new Audio('drum-sounds/hihat.wav');
+  audio.play()
+})
+
+var buttonElement = document.getElementById('tom')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+  var audio = new Audio('drum-sounds/tom.wav');
+  audio.play()
+})
+
+var buttonElement = document.getElementById('openhat')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+  var audio = new Audio('drum-sounds/openhat.wav');
+  audio.play()
+})
+
+var buttonElement = document.getElementById('boom')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+  var audio = new Audio('drum-sounds/boom.wav');
+  audio.play()
+})
+
+var buttonElement = document.getElementById('clap')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+  var audio = new Audio('drum-sounds/clap.wav');
+  audio.play()
+})
+
+var buttonElement = document.getElementById('kick')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+  var audio = new Audio('drum-sounds/kick.wav');
+  audio.play()
+})
+
+var buttonElement = document.getElementById('ride')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+  var audio = new Audio('drum-sounds/ride.wav');
+  audio.play()
+})
+
+var buttonElement = document.getElementById('snare')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+  var audio = new Audio('drum-sounds/snare.wav');
+  audio.play()
+})
+
+var buttonElement = document.getElementById('tink')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+  var audio = new Audio('drum-sounds/tink.wav');
+  audio.play()
+})
